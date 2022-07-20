945e8947543a30213c3a394ac9ebc7ebcad3953e
zoodogood <zoodogood@gmail.com>
2022-07-20 15:16:10 +0300

    snippet v1.0.0 for game mode structure

---
+++ modules/managers/LobbyManager/gameModes/.snippet.js
@@ -0,0 +1,18 @@
+
+class Mode {
+  static BUTTON = {
+    label: "LABEL",
+    value: "value === <MODE_FILE_NAME>",
+    description: "DESCRIPTION"
+  }
+
+  static onStart({game, interaction}){
+
+  }
+
+  static onEnd({game, interaction}){
+
+  }
+}
+
+export { Mode };
