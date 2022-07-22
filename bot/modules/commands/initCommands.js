8e0398eab77eb1efc23ae3a256d745de35adcb19
zoodogood <zoodogood@gmail.com>
2022-07-22 14:19:44 +0300

    none changes

--- bot/modules/commands/initCommands.js
+++ bot/modules/commands/initCommands.js
@@ -6,7 +6,7 @@ class CommandsLoader {
   async update(){
     const __dirname = `${ process.cwd() }`;
     globalThis.commands = new Collection();
-
+    
     const files = fileSystem.readdirSync(`${ __dirname }/bot/commands/`)
       .filter(name => /^[a-z].+?\.js/.test(name));
 
