7e9865b57a98d85f4e5b57a3ef7d72e5b51fb733
zoodogood <zoodogood@gmail.com>
2022-07-24 18:52:14 +0300

    new properties officialize: matchCount, wons, loses

--- modules/managers/UsersManager/User.js
+++ modules/managers/UsersManager/User.js
@@ -4,11 +4,15 @@
 
 
 class User {
-  constructor(id, {messages = 0, eloCoins = 50, mmr = 100}){
+  constructor(id, {messages = 0, eloCoins = 50, mmr = 100, matchWons = null, matchLoses = null, matchCount = null}){
     this.id = id;
     this.eloCoins = eloCoins;
     this.mmr = mmr;
     this.messages = messages;
+
+    this.matchCount = matchCount;
+    this.matchWons  = matchWons;
+    this.matchLoses = matchLoses;
   }
 }
 
