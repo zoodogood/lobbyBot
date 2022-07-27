e84f5d32c125ddcbfc8d5b038efae18b59059e31
zoodogood <zoodogood@gmail.com>
2022-07-27 11:08:45 +0300

    Сhange permissions for usage

--- bot/commands/create.js
+++ bot/commands/create.js
@@ -66,6 +66,7 @@ class Command extends BaseCommand {
     name: "create",
     slash: {
       description: "Создать лобби",
+      default_member_permissions: 8,
       options: [
         {
           type: 3,
