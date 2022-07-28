aa668aeb250f475f1c2338731f0f6518efc67db2
zoodogood <zoodogood@gmail.com>
2022-07-28 14:01:32 +0300

    Standartize resultsTable for all game modes

--- modules/managers/LobbyManager/LobbyGame.js
+++ modules/managers/LobbyManager/LobbyGame.js
@@ -1,6 +1,8 @@
-
+import GuildManager from '@managers/GuildManager';
 import ModesManager from '@managers/ModesManager';
 
+import DiscordUtil from '@bot/discord-util';
+const {MessageConstructor} = DiscordUtil;
 
 class LobbyGame {
   constructor(mode){
@@ -22,6 +24,38 @@ class LobbyGame {
 
   get modeInfo(){
     return ModesManager.list.get(this.mode);
+  }
+
+  showResultsTable({interaction, table}){
+    const guildData = GuildManager.getGuild(interaction.guild);
+
+    const channelId = guildData.rankStatsChannelId;
+
+    if (!channelId){
+      return;
+    }
+
+    const channelsCache = interaction.client.channels.cache;
+    const channel = channelsCache.get(channelId);
+
+    const description = Object.entries(table)
+      .map(([userId, {past, fresh, pastRank, freshRank}]) => {
+        let rankChunk = "";
+        if (pastRank !== freshRank){
+          const toString = (rank) => rank ? `<@&${ rank }>` : "N/A";
+          rankChunk = `**Ранг:** ${ toString(pastRank) } => ${ toString(freshRank) }`;
+        }
+
+        const mmr = `**Очки:** ${ past } ${ fresh > past ? "+" : "-" } ${ Math.abs(fresh - past) } = ${ fresh }`;
+
+        return `<@${ userId }> ${ mmr } ${ rankChunk }`;
+      })
+      .join("\n");
+
+    const message = new MessageConstructor({
+      description
+    });
+    channel.send(message);
   }
 }
 
